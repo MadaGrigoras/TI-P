f90b3fb38b5d42b6f0cf1559c755c5752f6692e5
Mada <Mada@10.19.0.19>
2017-04-10 23:38:06 +0300

    DBmanager

--- src/servicii/web/DBManager.java
+++ src/servicii/web/DBManager.java
@@ -4,12 +4,55 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-//import java.sql.Statement;
-//import java.util.ArrayList;
-//import java.util.List;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.sql.PreparedStatement;
 
 public class DBManager {
+	
+	private static final String URL = "jdbc:mysql://localhost:3306/my_database";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "";
+	private static final DBManager instance = new DBManager();
+	private Connection conn;
+	public static DBManager getInstance() {
+		return instance;
+	}
+	private DBManager() {
+		System.out.println("Loading driver...");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver loaded!");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException(
+					"Cannot find the driver in the classpath!", e);
+		}
+		try {
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public List<Employee> getEmployeeList() {
+		try (Statement st = conn.createStatement()) {
+			List<Employee> eList = new ArrayList<Employee>();
+			st.execute("select * from Student");
+			ResultSet rs = st.getResultSet();
+			while (rs.next()) {
+				Employee e = new Employee(rs.getInt("id_employee"),
+					rs.getString("e_companyId"), rs.getString("e_firstName"),
+					rs.getString("e_lastName"),rs.getInt("e_cnp"), rs.getString("employee_job"),rs.getInt("salary"),rs.getString("e_email"),rs.getString("e_password"));
+				eList.add(e);
+			}
+			// st.close();
+			return eList;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
 	public static boolean validate(String name, String pass) {        
         boolean status = false;
         Connection conn = null;
@@ -21,6 +64,7 @@ public class DBManager {
         String driver = "com.mysql.jdbc.Driver";
         String userName = "root";
         String password = "";
+        
         try {
             Class.forName(driver).newInstance();
             conn = DriverManager
